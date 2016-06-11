a2c38063ca419cbc5c363317e54b752e6c7a3043
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-12 00:56:53 +0100

    Added state change in PromptContainer for inputting username

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,17 +2,42 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+  onUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+  onSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username = ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      //go to /battle
+    } else {
+      //go to /playerTwo
+    }
+  },
   render: function () {
     return (
       <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={transparentBg}>
         <h1>{this.props.route.header}</h1>
         <div className='col-sm-12'>
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className='form-group'>
               <input
                 className='form-control'
                 placeholder='Github username'
-                type='text'/>
+                type='text'
+                onChange={this.onUpdateUser}
+                value={this.state.username} />
             </div>
             <div className='form-group col-sm-4 col-sm-offset-4'>
               <button
