106a5955bb0e05b5c6e1d6e122ed833da94eb8e0
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-11 23:39:09 +0100

    Created nested components for user, username and avator

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Junyuan Xue',
+  username: 'junyuanx',
+  image: 'https://avatars1.githubusercontent.com/u/16904324?v=3&s=460'
+};
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div> Hello ReactJS Program </div>
+      <div>
+        <a href={'http://www.github.com/' + this.props.username} >
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div> {this.props.name} </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDom.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
