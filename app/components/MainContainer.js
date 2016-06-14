b45d0234cdced604bc9f5db15263a219cd8d9158
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-14 16:03:27 +0100

    Fixed MainContainer

--- app/components/MainContainer.js
+++ app/components/MainContainer.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var styles = require('../styles');
 
-function MainContainer () {
+function MainContainer (props) {
   return (
-    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
-      {props.childen}
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      {props.children}
     </div>
   )
-};
+}
 
 module.exports = MainContainer;
