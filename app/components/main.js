259ffedc39ad5ac84097141c02d87703520071f3
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-17 09:53:18 +0100

    Added ReactCSSTransitionGroup in Main.js, used React.cloneElement to give this.props.children keys to enable transition

--- app/components/main.js
+++ app/components/main.js
@@ -1,11 +1,17 @@
 var React = require('react');
-var ReactRouter = require('react-router');
+var ReactCSSTransitonGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitonGroup
+          transitionName='appear'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitonGroup>
       </div>
     )
   }
